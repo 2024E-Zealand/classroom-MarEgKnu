61c8bfc1dd2c2a822e9c521c94d7733a12f35306
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-08-27 13:32:09 +0200

    ClassRoom class added

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public ClassRoom()
+        {
+            
+        }
+        public string ClassName { get; private set; }
+
+        public List<Student> StudentList { get; private set; }
+
+        public DateTime SemesterStart { get; private set; }
+    }
+}
